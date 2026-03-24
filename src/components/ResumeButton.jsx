056bd6f0487b3e0c4970e3d6de7428937632b2ca
zandonella <123271994+zandonella@users.noreply.github.com>
2026-03-23 23:48:47 -0700

    update theme to dark and emerald

--- src/components/ResumeButton.jsx
+++ src/components/ResumeButton.jsx
@@ -4,7 +4,7 @@ export default function ResumeButton() {
       <a
         href="/MichaelZandonellaResume.pdf"
         download
-        className="group flex items-center gap-2 rounded-xl bg-gray-950 p-3 px-4 outline-1 outline-gray-700 transition-colors duration-200 hover:z-50 hover:bg-gray-900 hover:shadow-[0px_0px_30px_20px] hover:ring-2 hover:shadow-indigo-950/50 hover:ring-indigo-900 focus-visible:z-50 focus-visible:bg-gray-900 focus-visible:shadow-[0px_0px_30px_20px] focus-visible:ring-2 focus-visible:shadow-indigo-950/50 focus-visible:ring-indigo-900"
+        className="group bg-foreground flex items-center gap-2 rounded-xl p-3 px-4 outline-1 outline-zinc-800 transition-all duration-200 hover:z-50 hover:bg-emerald-900/40 hover:shadow-[0px_0px_30px_20px] hover:ring-2 hover:shadow-emerald-950/50 hover:ring-emerald-900 focus-visible:z-50 focus-visible:bg-emerald-900/40 focus-visible:shadow-[0px_0px_30px_20px] focus-visible:ring-2 focus-visible:shadow-emerald-950/50 focus-visible:ring-emerald-900"
       >
         <span className="">Resume</span>
         <svg
@@ -17,7 +17,7 @@ export default function ResumeButton() {
           strokeWidth="2"
           strokeLinecap="round"
           strokeLinejoin="round"
-          className="group-hover:animate-bounceOrig h-5 w-5 stroke-white transition-colors duration-200 group-hover:stroke-indigo-500"
+          className="group-hover:animate-bounceOrig h-5 w-5 stroke-white transition-colors duration-200 group-hover:stroke-emerald-500"
         >
           <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
           <polyline points="7 10 12 15 17 10"></polyline>
--- src/components/SkillBox.jsx
+++ src/components/SkillBox.jsx
@@ -2,7 +2,7 @@ export default function SkillBox({ className, skillName }) {
   return (
     <>
       <div
-        className={`group z-0 rounded-xl bg-gray-950 p-3 text-center shadow-none outline-1 outline-gray-700 transition-colors duration-200 hover:z-50 hover:bg-gray-900 hover:shadow-[0px_0px_60px_30px] hover:ring-2 hover:shadow-indigo-950/50 hover:ring-indigo-900`}
+        className={`group bg-foreground z-0 rounded-xl p-3 text-center shadow-none outline-1 outline-gray-700 transition-colors duration-200 hover:z-50 hover:bg-emerald-900/40 hover:shadow-[0px_0px_60px_30px] hover:ring-2 hover:shadow-emerald-950/50 hover:ring-emerald-900`}
       >
         <div className="flex flex-col items-center gap-2">
           <div className="icon mt-2">
